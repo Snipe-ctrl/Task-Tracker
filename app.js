65f68efebc82aec28abcdbc060a0121a2ceac27b
Snipe-ctrl <joshuarodli19@gmail.com>
2024-12-18 15:21:09 -0800

    added mark-done functionality and reformatted json file

--- app.js
+++ app.js
@@ -25,15 +25,13 @@ function addTask(task) {
             newId = jsonData.length > 0 ? jsonData[jsonData.length - 1].id + 1 : 1;
 
             const newTask = { 
-                id: newId,
-                task: {
+                    id: newId,
                     taskName: task,
                     description: null,
-                    status: null,
+                    status: 'to-do',
                     createdAt: null,
                     updatedAt: null
-                }
-             }
+             };
 
             jsonData.push(newTask);
             const updatedJson = JSON.stringify(jsonData, null, 4)
@@ -56,16 +54,16 @@ function addTask(task) {
 function updateTask(id, updatedTask) {
     fs.readFile('tasks.json', 'utf8', (err, data) => {
         if (err) {
-            console.error('Error reading file:', err)
+            console.error('Error reading file:', err);
             return;
         }
 
         try {
-            let jsonData = JSON.parse(data);
-            const task = jsonData.find(task => task.id === id);
+            const jsonData = JSON.parse(data);
+            const task = jsonData.find(item => item.id === id);
 
             if (task) {
-                task.task.taskName = updatedTask;
+                task.taskName = updatedTask;
                 const updatedJson = JSON.stringify(jsonData, null, 4);
 
                 fs.writeFile('tasks.json', updatedJson, 'utf8', (err) => {
@@ -86,6 +84,41 @@ function updateTask(id, updatedTask) {
 }
 )}
 
+function markDone(id) {
+    fs.readFile('tasks.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error('Error reading file:', err);
+            return;
+        }
+
+        try {
+
+            const jsonData = JSON.parse(data);
+            const task = jsonData.find(item => item.id === id);
+
+            if (task) {
+                task.status = 'Completed';
+                updatedJson = JSON.stringify(jsonData, null, 4);
+                
+                fs.writeFile('tasks.json', updatedJson, 'utf8', (err) => {
+                    if (err) {
+                        console.error('Error writing file: ', err);
+                        return;
+                    } else {
+                        console.log(`"${task.taskName}" marked completed. (ID:${id})`);
+                    }
+                })
+            } else {
+                console.log(`Task with id: ${id} not found.`)
+            };
+
+
+        } catch (parseError) {
+            console.error('Error parsing JSON: ', parseError);
+        }
+    }
+)};
+
 // allows for "" and '' strings as inputs
 function parseInput(input) {
     const regex = /(?:[^\s"]+|"[^"]*"|'[^']*')+/g;
@@ -99,34 +132,38 @@ rl.on('line', (line) => {
     const args = parseInput(input);
     const command = args[0];
 
+    const restUpdate = args.slice(2);
+    const taskNameUpdate = restUpdate.join(' ');
+    const idArg = Number(args[1]);
+
     switch (command) {
 
         case 'add':
 
-        // variables for add function
-        const rest = args.slice(1);
-        const taskName = rest.join(' ');
+            // variables for add function
+            const rest = args.slice(1);
+            const taskName = rest.join(' ');
 
-        if (rest.length > 0) {
-            addTask('tasks.json', taskName);
-        } else {
-            console.log('No task added. Please provide a task name.');
-        }
-        break;
+            if (rest.length > 0) {
+                addTask('tasks.json', taskName);
+            } else {
+                console.log('No task added. Please provide a task name.');
+            }
+            break;
 
         case 'update':
 
-        // variables for update function
-        const restUpdate = args.slice(2);
-        const taskNameUpdate = restUpdate.join(' ');
-        const idArg = Number(args[1]);
+            if (restUpdate.length > 0) {
+                updateTask(idArg, taskNameUpdate);
+            } else {
+                console.log('No task added. Please provide a task name.');
+            }
+            break;
 
-        if (restUpdate.length > 0) {
-            updateTask(idArg, taskNameUpdate);
-        } else {
-            console.log('No task added. Please provide a task name.');
-        }
-        break;
+        case 'mark-done':
+
+            markDone(idArg);
+            break;
 
     default:
         console.log(`Unknown command: ${command}`);
